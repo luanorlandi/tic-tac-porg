057d6b8dc7a0d6f94e1e5cbff020d1dee7bb4245
luanorlandi <orlandi.luan@gmail.com>
2018-04-08 20:27:51 -0300

    Import Component

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Square from './Square';
 
-export default class Board extends React.Component {
+export default class Board extends Component {
   renderSquare(i) {
     return (
       <Square
--- src/components/Game.js
+++ src/components/Game.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Board from './Board';
 
-export default class Game extends React.Component {
+export default class Game extends Component {
   constructor(props) {
     super(props);
     this.state = {
