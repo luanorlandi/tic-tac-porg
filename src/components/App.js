45937b3342d3c2e8672ec49cd9ed8fb1debe814d
luanorlandi <orlandi.luan@gmail.com>
2018-05-19 21:32:21 -0300

    Add social media for Facebook

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,31 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import Game from './Game';
 import SocialMedia from './SocialMedia';
 import './App.css';
 import label from '../json/label';
 
-export default class App extends Component {
-  render() {
-    return (
-      <div>
-        <header>
-          <h1>{ label.appName }</h1>
-        </header>
-        <main>
-          <Game />
-        </main>
-        <footer>
-          <SocialMedia />
-          <span className='copyright'>
-            { label.author }
-          </span>
-        </footer>
-      </div>
-    );
-  }
+export const App = () => {
+  return (
+    <div>
+      <header>
+        <h1>{ label.appName }</h1>
+      </header>
+      <main>
+        <Game />
+      </main>
+      <footer>
+        <SocialMedia />
+        <span className='copyright'>
+          { label.footer.createdBy }
+          <a className='author'
+            href='https://github.com/luanorlandi'>
+            { label.footer.author }
+          </a>
+        </span>
+      </footer>
+    </div>
+  );
 }
+
+export default App;
--- src/components/SocialMedia.js
+++ src/components/SocialMedia.js
@@ -6,17 +6,31 @@ import label from '../json/label';
 export const SocialMedia = () => {
   return (
     <div className='links'>
-      <a className="github-button"
-        href="https://github.com/luanorlandi/tic-tac-porg"
-        data-show-count="true"
-        aria-label="Star luanorlandi/tic-tac-porg on GitHub">
-        { label.github }
-      </a>
-      <a className="twitter-share-button"
-        href="https://twitter.com/share?ref_src=twsrc%5Etfw"
-        data-show-count="true">
-        { label.twitter }
-      </a>
+      <div className='media fb-share-button'
+        data-href='https://luanorlandi.github.com/tic-tac-porg'
+        data-layout='button_count'
+        data-size='small'
+        data-mobile-iframe='true'>
+        <a className='fb-xfbml-parse-ignore'
+          href='https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fluanorlandi.github.com%2Ftic-tac-porg&amp;src=sdkpreparse'>
+          { label.footer.facebook }
+        </a>
+      </div>
+      <div className='media'>
+        <a className='twitter-share-button'
+          href='https://twitter.com/share?ref_src=twsrc%5Etfw'
+          data-show-count='true'>
+          { label.footer.twitter }
+        </a>
+      </div>
+      <div className='media'>
+        <a className='github-button'
+          href='https://github.com/luanorlandi/tic-tac-porg'
+          data-show-count='true'
+          aria-label='Star luanorlandi/tic-tac-porg on GitHub'>
+          { label.footer.github }
+        </a>
+      </div>
     </div>
   );
 }
